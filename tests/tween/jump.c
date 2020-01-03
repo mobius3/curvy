dba47a7cddc85dd4222ba0f5744de39db6bea25a
Leonardo <leonardo.guilherme@gmail.com>
2020-01-02 23:07:07 -0300

    adds more jump test

--- tests/tween/jump.c
+++ tests/tween/jump.c
@@ -3,8 +3,21 @@
 
 Test(tween, jump_initializes_tween) {
   struct cy_tween tween = {};
-  cr_expect(tween.initialized == CY_FALSE);
-
   cy_seek(&tween, 0);
   cr_assert(tween.initialized == CY_TRUE);
+}
+
+Test(tween, jump_changes_duration) {
+  struct cy_tween tween = {
+      .from = 0,
+      .to = {
+          {.value = 1, .during = 100}
+      },
+  };
+
+  cy_jump(&tween, 1);
+  cr_assert(tween.current_duration == 100);
+
+  cy_jump(&tween, 0);
+  cr_assert(tween.current_duration == 0);
 }
