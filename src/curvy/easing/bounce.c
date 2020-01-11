6b9bead5ffc7af12c83239dcdc16fa900b7c0270
Leonardo <leonardo.guilherme@gmail.com>
2020-01-11 00:30:09 -0300

    tests and implements bounce(in/out) easing

--- src/curvy/easing/bounce.c
+++ src/curvy/easing/bounce.c
@@ -1,13 +1,26 @@
 #include "curvy/easing/bounce.h"
 
 float cy_bounce(float p, float start, float end) {
-  return 0;
+  if (p < 0.5f) return (float)(cy_bounce_in(p * 2, 0, end) * .5f + start);
+  else return (float)(cy_bounce_out((p * 2 - 1), 0, end) * .5f + (end - start) * .5f + start);
 }
 
 float cy_bounce_in(float p, float start, float end) {
-  return 0;
+  return (end - start) - cy_bounce_out((1 - p), 0, end) + start;
 }
 
 float cy_bounce_out(float p, float start, float end) {
-  return 0;
+  float c = end - start;
+  if (p < (1 / 2.75f)) {
+    return (float) (c * (7.5625f * p * p) + start);
+  } else if (p < (2.0f / 2.75f)) {
+    float postFix = p -= (1.5f / 2.75f);
+    return (float) (c * (7.5625f * (postFix) * p + .75f) + start);
+  } else if (p < (2.5f / 2.75f)) {
+    float postFix = p -= (2.25f / 2.75f);
+    return (float) (c * (7.5625f * (postFix) * p + .9375f) + start);
+  } else {
+    float postFix = p -= (2.625f / 2.75f);
+    return (float) (c * (7.5625f * (postFix) * p + .984375f) + start);
+  }
 }
--- tests/easing/all-easings.c
+++ tests/easing/all-easings.c
@@ -1,5 +1,6 @@
 #include <curvy/easing/linear.h>
 #include <curvy/easing/back.h>
+#include <curvy/easing/bounce.h>
 
 #include "criterion/criterion.h"
 #include "test-easing-fn.h"
@@ -8,6 +9,7 @@ Test(easing, linear_easing_return_expected_values) {
   float at[] = { 0, 0.5f, 1.0f }, expected[] = { 0.0f, 0.5f, 1.0f };
   test_easing_fn(cy_linear, 0, 1, at, expected, 3);
 }
+
 
 Test(easing, back_easing_return_expected_values) {
   float at[] = { 0, 0.1f, 0.5f, 0.9f, 1.0f }, expected[] = { 0.000000f, -0.037519f, 0.500000f, 1.037519f, 1.000000f };
@@ -19,8 +21,34 @@ Test(easing, back_in_easing_return_expected_values) {
   test_easing_fn(cy_back_in, 0, 1, at, expected, 5);
 }
 
-
 Test(easing, back_out_easing_return_expected_values) {
   float at[] = { 0, 0.1f, 0.5f, 0.9f, 1.0f }, expected[] = { 0.000000f, 0.408828f, 1.087698f, 1.014314f, 1.000000f };
   test_easing_fn(cy_back_out, 0, 1, at, expected, 5);
+}
+
+Test(easing, bounce_easing_return_expected_values) {
+  float at[] = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f },
+      expected[] = {
+      0.000000f, 0.030000f, 0.113750f, 0.045000f, 0.348750f, 0.500000f,
+      0.651250f, 0.955000f, 0.886250f, 0.970000f, 1.000000f
+  };
+  test_easing_fn(cy_bounce, 0, 1, at, expected, 11);
+}
+
+Test(easing, bounce_in_easing_return_expected_values) {
+  float at[] = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f },
+      expected[] = {
+      0.000000f, 0.011875f, 0.060000f, 0.069375f, 0.227500f, 0.234375f,
+      0.090000f, 0.319375f, 0.697500f, 0.924375f, 1.000000f
+  };
+  test_easing_fn(cy_bounce_in, 0, 1, at, expected, 11);
+}
+
+Test(easing, bounce_out_easing_return_expected_values) {
+  float at[] = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f },
+  expected[] = {
+      0.000000f, 0.075625f, 0.302500f, 0.680625f, 0.910000f, 0.765625f,
+      0.772500f, 0.930625f, 0.940000f, 0.988125f, 1.000000f
+  };
+  test_easing_fn(cy_bounce_out, 0, 1, at, expected, 11);
 }
