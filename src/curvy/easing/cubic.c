6653261588c0a66a8840715d261961172b684ac5
Leonardo <leonardo.guilherme@gmail.com>
2020-02-07 22:55:06 -0300

    tests and implements cubic(in/out) easing

--- src/curvy/easing/cubic.c
+++ src/curvy/easing/cubic.c
@@ -1,13 +1,19 @@
 #include "curvy/easing/cubic.h"
 
 float cy_cubic(float p, float start, float end) {
-  return 0;
+  p *= 2;
+  if (p < 1) {
+    return ((end - start) / 2) * p * p * p + start;
+  }
+  p -= 2;
+  return ((end - start) / 2) * (p * p * p + 2) + start;
 }
 
 float cy_cubic_in(float p, float start, float end) {
-  return 0;
+  return (end - start) * p * p * p + start;
 }
 
 float cy_cubic_out(float p, float start, float end) {
-  return 0;
+  --p;
+  return (end - start) * (p * p * p + 1) + start;
 }
--- tests/easing/all-easings.c
+++ tests/easing/all-easings.c
@@ -2,6 +2,7 @@
 #include <curvy/easing/back.h>
 #include <curvy/easing/bounce.h>
 #include <curvy/easing/circular.h>
+#include <curvy/easing/cubic.h>
 
 #include "criterion/criterion.h"
 #include "test-easing-fn.h"
@@ -73,4 +74,25 @@ Test(easing, circular_out_easing_return_expected_values) {
   expected[] = { 0.000000f, 0.435890f, 0.600000f, 0.714143f, 0.800000f, 0.866025f,
                      0.916515f, 0.953939f, 0.979796f, 0.994987f, 1.000000f };
   test_easing_fn(cy_circular_out, 0, 1, at, expected, 11);
+}
+
+Test(easing, cubic_easing_return_expected_values) {
+  float at[] = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f },
+      expected[] = { 0.000000f, 0.004000f, 0.032000f, 0.108000f, 0.256000f, 0.500000f,
+                     0.744000f, 0.892000f, 0.968000f, 0.996000f, 1.000000f };
+  test_easing_fn(cy_cubic, 0, 1, at, expected, 11);
+}
+
+Test(easing, cubic_in_easing_return_expected_values) {
+  float at[] = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f },
+      expected[] = { 0.000000f, 0.001000f, 0.008000f, 0.027000f, 0.064000f, 0.125000f,
+                     0.216000f, 0.343000f, 0.512000f, 0.729000f, 1.000000f };
+  test_easing_fn(cy_cubic_in, 0, 1, at, expected, 11);
+}
+
+Test(easing, cubic_out_easing_return_expected_values) {
+  float at[] = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f },
+      expected[] = { 0.000000f, 0.271000f, 0.488000f, 0.657000f, 0.784000f, 0.875000f,
+                     0.936000f, 0.973000f, 0.992000f, 0.999000f, 1.000000f };
+  test_easing_fn(cy_cubic_out, 0, 1, at, expected, 11);
 }
