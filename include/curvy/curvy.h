c423f314af629186ef847180be2262ecd501b0ad
Leonardo <leonardo.guilherme@gmail.com>
2020-03-23 13:43:52 -0300

    create mainpage on curvy.h

--- include/curvy/curvy.h
+++ include/curvy/curvy.h
@@ -1,6 +1,26 @@
 #ifndef CURVY_H
 #define CURVY_H
 
+/**
+ * @mainpage Curvy
+ *
+ * Curvy is a C99 library that delivers a clear and straightforward
+ * interpolation API, allowing developers to build fluid and creative
+ * animations for their software.
+ *
+ * It has:
+*
+* - Small, clear and declarative-style APIs
+* - Many built-in easing functions
+* - Multi-point interpolation
+* - No external dependencies
+* - Zero allocations
+* - Tween stepping, seeking and jumping
+* - Support for step, seek and jump callbacks
+ *
+ * This is it's API documentation.
+ */
+
 #include "curvy/tween.h"
 #include "curvy/easings.h"
 
